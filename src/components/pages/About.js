940f181c14749b34a52b2c6ebda90cdbf4642284
Sarim Zia <username@email.com>
2021-01-07 21:23:11 -0500

    Updated Todo website and deployed

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <h3>This is the ToDo List app v1.0.0. You can use it to manage your daily tasks.
+            </h3>
+        </div>
+    )
+}
+
